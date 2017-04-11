f7c34a030b0bf0be9e3eaeadeb4d98fef63456ef
Esperanza Moreno Cruz <hola@mi-mina.com>
2017-04-11 19:53:14 +0200

    practicas

--- practicas/practica00/sketch.js
+++ practicas/practica00/sketch.js
@@ -9,37 +9,22 @@
 
 function setup() {
   // Creamos el canvas
-  createCanvas(800, 400);
-  // Creamos un background que no es más que un rectángulo del mismo tamaño
-  // que el canvas con un color
-  // Un sólo número es un tono de gris
-  background(200);
-  // Tres número entre 0 y 255 en RGB
-  // background(200, 255, 255);
-  
-  // Todo lo que queremos aplicarle a una figura o a varias
-  // hay que ponerlo antes
-  
-  // Le damos un color al perímetro
-  stroke(255, 255, 0);
-  // Le damos un grosor al perímetro
-  strokeWeight(5);
-  // Le damos un color al relleno
-  fill(255, 0, 0);
-  // Dibujamos un rectángulo
-  rect(100, 100, 100, 50);
+  createCanvas(400, 400); // Aquí también puedo poner un comentario
 
-  noFill();
-  stroke('darkMagenta');
-  // Dibujamos un círculo
-  ellipse(300, 125, 50, 50)
+  // Paredes
+  rect(100, 200, 150, 150);
 
-  noStroke();
-  fill(50, 200, 100);
-  // Dibujamos un triángulo: triangle(X1, Y1, X2, Y2, X3, Y3) 
-  triangle(100, 200, 200, 200, 150, 250);
+  // Tejado
+  // triangle(X1, Y1, X2, Y2, X3, Y3) 
+  triangle(100, 200, 250, 200, 175, 100);
+  
+  // Puerta
+  rect(150, 250, 50, 100);
+ 
+  // Luna
+  ellipse(300, 125, 50, 50);
+
   
   // Para imprimir en la consola de la IDE
-  print('hola');
-  
+  print('hola, esta es mi casa');
 }
--- practicas/practica01/sketch.js
+++ practicas/practica01/sketch.js
@@ -1,23 +1,41 @@
-// Variables y tipos de datos
+// Formas simples con colores
 
-// Javascript es case-sensitive: distingue entre minúsculas y mayúsculas
-// Se usa el estilo Camelcase para nombrar a las variables
+function setup() {
+  // Creamos el canvas
+  createCanvas(400, 400);
+  // Creamos un background que no es más que un rectángulo del mismo tamaño
+  // que el canvas con un color
+  // Un sólo número es un tono de gris
+  background(200);
+  // Tres número entre 0 y 255 en RGB
+  // background(200, 255, 255);
+  
+  // Todo lo que queremos aplicarle a una figura o a varias
+  // hay que ponerlo antes
 
-// Variable global declarada fuera de una función. La podemos usar
-// en cualquier sitio
-// Statements - sentencias, acaban con punto y coma
-var miEdad = 40;
+  // Paredes
+  fill(0, 80, 80);
+  noStroke();
+  rect(100, 200, 150, 150);
 
-// Distinguir dos pasos: declarar y asignar valor
-// palabra clave var para declarar una variable
-var miNombre;
-// Asignar un valor a una variable con el signo igual
-miNombre = 'Esperanza';
-miEdad = 25;
+  // Tejado
+  fill(250, 150, 0);
+  stroke(250, 100, 0);
+  strokeWeight(15);
+  // triangle(X1, Y1, X2, Y2, X3, Y3) 
+  triangle(100, 200, 250, 200, 175, 100);
+  
+  // Puerta
+  fill(0, 200, 200);
+  noStroke();
+  rect(150, 250, 50, 100);
+ 
+  // Luna
+  noStroke();
+  fill('white');
+  ellipse(300, 125, 50, 50);
 
-function setup () {
-  print('Me llamo ' + miNombre + ' y mi edad es ' + miEdad + ' años');
-  print(typeof(miEdad))
-  print(typeof(miNombre))
+  
+  // Para imprimir en la consola de la IDE
+  print('hola, esta es mi casa');
 }
-
