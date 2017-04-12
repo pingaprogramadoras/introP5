c8897f1a905365b38926d772c0781ffa7d0c7cba
Esperanza Moreno Cruz <hola@mi-mina.com>
2017-04-12 16:37:21 +0200

    practicas

---
+++ practicas/practica04bis/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  colorMode(HSB);
+  background(0, 0, 0, 0.05);
+  strokeWeight(20);
+  stroke(mouseX, 100, 100);
+  line(mouseX, 0, mouseX, 400);
+  stroke(mouseX, 100, 100);
+  line(0, mouseY, 400, mouseY);
+}
