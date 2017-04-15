73eff0b1cfc15e376f294435eec47dafca24cfef
Esperanza Moreno Cruz <hola@mi-mina.com>
2017-04-15 17:24:17 +0200

    practicas

--- practicas/practica18/sketch.js
+++ practicas/practica18/sketch.js
@@ -1,27 +1,21 @@
-var ondaCoseno = [];
+// Loops anidados
 
-function setup() {
-  createCanvas(720, 360);
+var setup = function() {
+  createCanvas(600, 400);
 
-  // Añadimos valores al array ondaCoseno
-  for (var i = 0; i < width; i++) {
-    var amount = map(i, 0, width, 0, PI);
-    ondaCoseno[i] = abs(cos(amount));
+  for (var x = 0; x < 5; x++) {
+    for (var y = 0; y < 5; y++) {
+      console.log('x: ' + x + ', y: ' + y);
+    }
   }
+};
 
-  background(255);
-  
-  var y1 = 0;
-  var y2 = height/2;
-  for (var j = 0; j < width; j +=1) {
-    stroke(ondaCoseno[j] * 255, 0, 0);
-    line(j, y1, j, y2);
+var draw = function() {
+  background(0);
+
+  for (var i = 0; i <= width; i += 20) {
+    for (var j = height / 3; j <= height * 2 / 3; j += 20) {
+      ellipse(i, j, 10, 10);
+    }
   }
-  
-  y1 = y2;
-  y2 = height;
-  for (var k = 0; k < width; k+=1) {
-    stroke(255 - ondaCoseno[k]*255, 0, 0);
-    line(k, y1, k, y2);
-  }
-}
+};
